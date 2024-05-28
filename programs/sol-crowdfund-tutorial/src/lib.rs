4cccf9db695bc333b3d09f21c0c3240430ed1236
TinoDCodes <tinodauti@gmail.com>
2024-05-28 16:51:15 +0200

    feat: withdraw function & context added

--- programs/sol-crowdfund-tutorial/src/lib.rs
+++ programs/sol-crowdfund-tutorial/src/lib.rs
@@ -4,15 +4,37 @@ declare_id!("2N6QZDfjFzCKYVJ9pELvv3S5y1KVxxDTyzDuyAdv5RaV");
 
 #[program]
 pub mod sol_crowdfund_tutorial {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+
     use super::*;
 
-    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> ProgramResult {
         let campaign = &mut ctx.accounts.campaign;
         campaign.name = name;
         campaign.description = description;
         campaign.amount_donated = 0;
         campaign.admin = ctx.accounts.user.key();
 
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        let user = &mut ctx.accounts.user;
+
+        // check if user trying to withdraw is the campaign admin
+        if campaign.admin != *user.key {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+
+        // check if the campaign account has enough funds to withdraw
+        let rent_balance = Rent::get()?.minimum_balance(campaign.to_account_info().data_len());
+        if **campaign.to_account_info().lamports.borrow() - rent_balance < amount {
+            return Err(ProgramError::InsufficientFunds);
+        }
+
+        **campaign.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **user.to_account_info().try_borrow_mut_lamports()? += amount;
         Ok(())
     }
 }
@@ -24,6 +46,14 @@ pub struct Create<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>
 }
 
 #[account]
